a2c827e708ff7fb4a020eab8df54c4a2eceadc8d
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-07-20 10:03:49 -0300

    Section 3: Fundamentos 20. Comentários

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,33 @@
 package fundamentos;
 
+/**
+ * Essa classe representa
+ *
+ * @author Davi Rodriguez <email@email.com>
+ * @since JDK1.8
+ */
 public class PrimeiroPrograma {
 
+    /**
+     * Todoo programa Java inicia pelo main
+     *
+     * @param args seila, deve ser os argumentos de inicio
+     *
+     */
     public static void main(String[] args) {
+        // Acho que o public é pq todo mundo pode importar
+        // static seila
+        // void pq não retorna nada
+        // Uma sentença de código termina com ;
+        // Mais um comentário...
+        // Fim
         System.out.println("Primeiro programa parte #01!");
-        System.out.println("Primeiro programa parte #02!");
+
+        /*
+        Linha 1
+        Linha 2
+        Linha 3...
+         */
+        System.out.println("Fim");  // Aqui tb funciona
     }
 }
