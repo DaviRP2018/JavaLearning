01ee7a464b0cd764e6f23c1db5caef8f624264b6
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-07-20 10:30:20 -0300

    Section 3: Fundamentos 23. Desafio variáveis e constantes - Resposta

--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -3,14 +3,27 @@ package fundamentos;
 public class Temperatura {
     public static void main(String[] args) {
         // (°F - 32) x 5/9 = °C
-        final int AJUSTE = 32;
+        final int CONST = 32;
         final double MULTIPLICADOR = (double) 5 / 9;
 
         double farenheit = 90;
         double celsius;
 
-        celsius = (farenheit - AJUSTE) * MULTIPLICADOR;
+        celsius = (farenheit - CONST) * MULTIPLICADOR;
 
         System.out.println(celsius);
+
+        // Resposta do prof
+        // Eu posso colocar 5.0 / 9 que o result vai ser double
+        final double FATOR = 5.0 / 9.0;
+        final double AJUSTE = 32;
+
+        double fahrenheit = 86;
+        double result = (fahrenheit - AJUSTE) * FATOR;
+        System.out.println("O resultado é " + result + "°C.");
+
+        fahrenheit = 32;
+        result = (fahrenheit - AJUSTE) * FATOR;
+        System.out.println("O resultado é " + result + "°C.");
     }
 }
