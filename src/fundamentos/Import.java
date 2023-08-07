0e78bca3fe23853d6fac05654f60e68f8949cd19
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-07 19:45:46 -0300

    Section 3: Fundamentos 28. Import

--- src/fundamentos/Import.java
+++ src/fundamentos/Import.java
@@ -1,6 +1,23 @@
 package fundamentos;
 
+import java.util.Date;
+
+import javax.swing.*;  // wild card aparentemente
+
 public class Import {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        java.lang.String oi = "macaco";
+
+        String s = "Bom dia!";
+        System.out.println(s);
+
+
+
+        Date d = new Date();
+        System.out.println(d);
+
+        JButton botao = new JButton();
+
+    }
 }
