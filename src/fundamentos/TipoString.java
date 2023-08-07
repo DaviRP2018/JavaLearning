117991456a5770d485f37e75af140f5408df23f1
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-07 20:00:56 -0300

    Section 3: Fundamentos 29. Tipo String

---
+++ src/fundamentos/TipoString.java
@@ -0,0 +1,44 @@
+package fundamentos;
+
+public class TipoString {
+
+    public static void main(String[] args) {
+        System.out.println("Olá pessoal".charAt(2));
+        System.out.println("Olá pessoal".charAt(0));
+
+        String s = "Boa tarde";
+        s = "Bom dia";
+        System.out.println(s.concat("!!!"));
+        System.out.println(s + "!!!");
+
+        System.out.println(s.startsWith("Bom"));
+        System.out.println(s.startsWith("bom"));
+        System.out.println(s.toLowerCase().startsWith("bom") + "\n");
+        System.out.println();
+        System.out.println(s.endsWith("dia"));
+
+        System.out.println(s.length());
+
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var idade = 33;
+        var salario = 1234.45F;
+
+        System.out.println(
+            "Nome: "
+            + nome
+            + "\nSobrenome: "
+            + sobrenome
+            + "\nIdade: "
+            + idade
+            + "\nSalário: "
+            + salario);
+
+        System.out.printf("Nome: %s %s\nSalário: %.2f\n", nome, sobrenome, salario);
+
+        String frase = String.format("Tiozão: %s", nome);
+        System.out.println(frase);
+
+        System.out.println(frase.substring(0, 6));
+    }
+}
