45806044826059ee7da662095f58097e2b7914b0
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-09 20:10:00 -0300

    Section 3: Fundamentos 56. Java vs Javac

---
+++ src/JavaJavac.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class JavaJavac {
+
+    public static void main(String[] args) {
+        System.out.println("Olá terminal do computador");
+
+        /*
+                Scanner teclado = new Scanner(System.in);
+
+                String valor = teclado.nextLine();
+                System.out.println("Valor é " + valor);
+                teclado.close();
+        */
+
+        System.out.println(Arrays.toString(args));
+    }
+}
