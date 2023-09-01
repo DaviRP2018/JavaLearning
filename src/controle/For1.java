f2977efe07c9a40c36850fe0110e88b4e5f83858
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-31 22:13:38 -0300

    Section 4: Estruturas de Controle 71. FOR #01

---
+++ src/controle/For1.java
@@ -0,0 +1,26 @@
+package controle;
+
+public class For1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++)
+            System.out.printf("i = %d\n", i);
+
+        int x = 2;
+        for (; x < 10;) {
+            System.out.println("x = " + x);
+            x++;
+        }
+
+        for(;;) {
+            System.out.println("Infinito");
+            break;
+        }
+
+        for(;true;) {
+            System.out.println("Infinito");
+            break;
+        }
+    }
+}
